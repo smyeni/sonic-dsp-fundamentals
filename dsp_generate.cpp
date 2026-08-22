3f41154f53e0f2ff844b25b1b299cf2068103040
Sifiso Myeni <smyeni@soniclabs.com>
2026-08-22 14:16:30 +0200

    More DSP coding

--- dsp_generate.cpp
+++ dsp_generate.cpp
@@ -19,12 +19,13 @@ int main()
 	const double pi = std::numbers::pi;
 	const float f_digital = 2*pi*freq/F_s;
 
-	std::println("{:>3}, {:>12}, {:>12}", "index", "phase", "x[n]");
-    for (auto [index, phase_val] : std::views::enumerate(phase)) {
-		phase[index] = index*f_digital;
-        x[index] = std::sin(phase[index]);
-	    std::println("{:>3}, {:>14.6e}, {:.6e}", index, phase_val, x[index]);
-    }
+	std::println("{:>3},{:>12},{:>12}", "index", "phase", "x[n]");
+	for (std::size_t n = 0; n < phase.size(); ++n) 
+	{
+		phase[n] = n * f_digital;
+		x[n] = std::sin(phase[n]);
+		std::println( "{:>3}, {:>14.6e}, {:>14.6e}", n, phase[n], x[n] );
+	}
 
 	std::ofstream file{"signal.csv"};
 	file.imbue(std::locale::classic());
